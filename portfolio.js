f2fb649e8f3877a5b081e0d0714cd788cf71f173
sainip1 <sainip1@wit.edu>
2023-06-08 15:17:24 -0400

    added a responsive header, trying to add a menu click feature that shows mobile nav bar

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,8 @@
 console.log('Hello World!');
+
+const menuButton = document.getElementById("menu-button");
+const nav = document.getElementById("mobile-nav");
+
+menuButton.addEventListener("click", function(){
+    menuButton.innerHTML = "clicked";
+});
