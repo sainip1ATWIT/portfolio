1c2eec12f101f01da8fc38ef270a4aff10af2278
sainip1 <sainip1@wit.edu>
2023-06-10 20:49:45 -0400

    finished header

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,14 @@
+//Document Objects
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const bar = document.querySelectorAll(".bar");
 
+//EventListeners
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
+hamburger.addEventListener("mouseover", grayBackground);
+hamburger.addEventListener("mouseleave", redBackground);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
@@ -13,4 +18,16 @@ function mobileMenu() {
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}
+
+function grayBackground() {
+    bar.forEach(b => {
+        b.style.backgroundColor = "#475569";
+    });
+}
+
+function redBackground() {
+    bar.forEach(b => {
+        b.style.backgroundColor = "#a42121";
+    });
 }
